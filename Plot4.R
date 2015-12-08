2a47da875c6411b87a834e6bfa2e2ab338aa9555
pbcomita <paulcomita@sbcglobal.net>
2015-12-08 11:21:34 -0800

    update2

--- Plot4.R
+++ Plot4.R
@@ -7,7 +7,7 @@ subpower1 <- subset(powerdata, powerdata$Date =="1/2/2007" | powerdata$Date =="2
 subdattime <- paste(subpower1$Date, subpower1$Time)
 subpower1$Date <- strptime(subdattime, "%d/%m/%Y  %H:%M:%S")
 ##set parameters to plot all four plots
-par(mfrow =c(2,2), mar = c(4,4,2,2), oma = c(0,0,0,0))
+par(mfrow =c(2,2), mar = c(4,4,1,1), oma = c(0,0,0,0))
 with(subpower1, {
         plot(Date, Global_active_power, type = "l", lty=1, xlab="", ylab="Global Active Power (kilowatts)")
         plot(Date, Voltage, type = "l", lty=1, xlab="datetime", ylab="Voltage")
@@ -15,7 +15,7 @@ with(subpower1, {
                 lines(Date, Sub_metering_2, type = "l", lty=1, col="red")
                 lines(Date, Sub_metering_3, type = "l", lty=1, col="blue")
                 legend("topright", lty=1, col=c("black", "red", "blue"), bty="n", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.8, text.width = strwidth("Sub_metering_1        "))
-        plot(Date, Global_reactive_power, type = "l", lty=1, xlab="datetime", ylab="Global_reactive_power")
+        plot(Date, Global_reactive_power, type = "l", lty=1, xpd = TRUE, xlab="datetime", ylab="Global_reactive_power")
 })
-dev.copy(png, file = "plot4.png", width = 480, height = 480)
-dev.off(4)
+#dev.copy(png, file = "plot4.png", width = 480, height = 480)
+#dev.off(4)
